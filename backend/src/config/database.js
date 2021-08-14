a697e4f2a3e79b4cc097f875dca6034a0e5dbe9f
brendo <brendoja@gmail.com>
2021-08-13 23:12:14 -0300

    inicializando estrutura

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username:'postgres',
+    password:'root',
+    database:'tcc-cliente-vendas',
+    define: {
+        timestamp: true,
+        underscored: true
+    },
+
+};
