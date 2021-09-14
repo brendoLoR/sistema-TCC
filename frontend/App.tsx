15a4c1e6beacc88dcb0a879f92e6a9344d14e2ed
Victor Cassiano <43105344+Victorcassiano@users.noreply.github.com>
2021-09-13 23:00:24 -0300

    Adicionado Toast e tsconfig

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,30 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, View, Text } from 'react-native';
+import { ToastProvider } from 'react-native-toast-notifications'
 import { NavigationContainer } from '@react-navigation/native';
-
+import Toast from '../frontend/src/Components/Toast'
 import Routes from './src/routes';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <Routes />
+      <ToastProvider
+        animationType="zoom-in"
+        renderType={{
+          customSuccess: ({ data, id }) => (
+            <Toast
+              id={id}
+              message={data.message}
+              title={data.title}
+              type={data.type}
+            />
+          )
+        }}
+      >
+        <Routes />
+      </ToastProvider>
     </NavigationContainer>
   );
 }
