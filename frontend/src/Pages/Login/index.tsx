5c5526eb4c57f05be74c681d53cd60fd2754ea1b
Victor Cassiano <43105344+Victorcassiano@users.noreply.github.com>
2021-10-17 18:47:03 -0300

    Adicionado novo input

--- frontend/src/Pages/Login/index.tsx
+++ frontend/src/Pages/Login/index.tsx
@@ -1,18 +1,30 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { KeyboardAvoidingView, Platform, Animated, View } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { KeyboardAvoidingView, Animated, View } from 'react-native';
+import { Form } from '@unform/mobile';
 import { useNavigation } from '@react-navigation/native';
-import { Button, Input } from 'react-native-elements'
+import { Button } from 'react-native-elements'
+import Input from '../../Components/input'
+import * as Yup from 'yup';
 import normalize from 'react-native-normalize'
 import { useToast } from "react-native-toast-notifications";
 
-import { ButtonSignUp, Container, Text } from './styles'
+import { ButtonSignUp, Text } from './styles'
 
 import fonts from '../../Theme/fonts';
 import Logo from '../../assets/Logo.png';
+import { FormHandles } from '@unform/core';
+import { TextInput } from 'react-native-gesture-handler';
+import getValidationErrors from '../../utils/getValidationErrors';
+import c from 'react-native-toast-notifications';
+import Toast from 'react-native-toast-notifications/lib/typescript/toast';
 
 const Login: React.FC = () => {
   const { navigate } = useNavigation();
   const Toast = useToast();
+
+  const formRef = useRef<FormHandles>(null);
+  const inputEmailRef = useRef<TextInput>(null);
+  const inputPasswordRef = useRef<TextInput>(null);
 
   const [translatedLogo] = useState(new Animated.Value(normalize(220, 'height')))
   const [scale] = useState(new Animated.Value(0))
@@ -20,24 +32,81 @@ const Login: React.FC = () => {
   const [loadingLogin, setLoadingLogin] = useState(false);
 
   const handleLogin = useCallback(
-    async () => {
-      setLoadingLogin(true)
-      setTimeout(() => {
-        setLoadingLogin(false)
-        navigate('Home');
-      }, 100)
-      Toast.show('',
-        {
-          id: String(Math.random()),
-          placement: 'top',
-          type: 'customSuccess',
-          data: {
-            title: 'Olá',
-            message: 'MundoMundoMundoMundoMundoMundoMundoMundoMundoMundoMundo',
-            type: 'success'
+    async (data: object) => {
+      try {
+        setLoadingLogin(true)
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail é obrigatório')
+            .email('Por favor, digite um e-mail válido'),
+          password: Yup.string().min(8, 'sua senha precisa ter no mínimo 8 dígitos').required('Senha é obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        setTimeout(() => {
+          setLoadingLogin(false)
+          navigate('Home');
+        }, 1000)
+
+
+      } catch (err) {
+        setLoadingLogin(false);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+
+          if (errors.email && errors.password) {
+            Toast.show('',
+              {
+                placement: 'top',
+                type: 'customSuccess',
+                data: {
+                  title: 'Ops!',
+                  message: 'Os campos estão vazios',
+                  type: 'error'
+                }
+              }
+            )
+            formRef.current?.setErrors(errors)
+            return;
+          }
+
+          if (errors.email) {
+            Toast.show('',
+              {
+                placement: 'top',
+                type: 'customSuccess',
+                data: {
+                  title: 'Ops!',
+                  message: errors.email,
+                  type: 'error'
+                }
+              }
+            )
+            formRef.current?.setFieldError('email', errors.email)
+            return;
+          }
+          if (errors.password) {
+            Toast.show('',
+              {
+                placement: 'top',
+                type: 'customSuccess',
+                data: {
+                  title: 'Ops!',
+                  message: errors.password,
+                  type: 'error'
+                }
+              }
+            )
+            formRef.current?.setFieldError('password', errors.password)
+            return;
           }
         }
-      )
+      }
     },
     [],
   );
@@ -58,8 +127,8 @@ const Login: React.FC = () => {
   return (
     <KeyboardAvoidingView style={{ flex: 1 }}
       behavior="position"
+      keyboardVerticalOffset={normalize(-40, 'height')}
       enabled
-      keyboardVerticalOffset={Platform.OS === 'ios' ? normalize(40, 'height') : normalize(-30, 'height')}
     >
       <Animated.Image
         source={Logo}
@@ -79,21 +148,19 @@ const Login: React.FC = () => {
           transform: [{ scale }]
         }}
       >
-        <Input
-          leftIcon={{ type: 'Feather', name: 'mail' }}
-          placeholder="email@endereco.com"
-          label="E-mail"
-          labelStyle={{ fontFamily: fonts.light, fontSize: 14, color: '#111' }}
-        />
-        <Input
-          leftIcon={{ type: 'Feather', name: 'lock', color: '#111' }}
-          labelStyle={{ fontFamily: fonts.light, fontSize: 14, color: '#111' }}
-          containerStyle={{ borderColor: 'red' }}
-          placeholder="senha"
-          label="Senha"
-          secureTextEntry
-          errorMessage=""
-        />
+        <Form
+          style={{
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: 50,
+          }}
+          ref={formRef}
+          onSubmit={handleLogin}
+        >
+          <Input name="email" label="E-mail" placeholder="endereco@email.com" startIcon="mail" />
+          <Input name="password" label="Senha" placeholder="senha" isPassword startIcon="lock" />
+        </Form>
         <Button
           title="Entrar"
           titleStyle={{ fontFamily: fonts.regular, fontSize: 18 }}
@@ -111,7 +178,7 @@ const Login: React.FC = () => {
             borderRadius: 10,
           }}
           loading={loadingLogin}
-          onPress={handleLogin}
+          onPress={() => formRef.current?.submitForm()}
         />
         <View style={{ flexDirection: 'row' }}>
           <Text>Você ainda não possui cadastro?</Text>
