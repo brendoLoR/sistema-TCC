d9e7e4c9ff3a16a197c86f910d20d32b8bdd1584
Brendo Silva <61713673+brendoLoR@users.noreply.github.com>
2022-01-12 12:48:51 -0300

    arrumando os testes

--- backend/__tests__/cpfvalidator.test.js
+++ backend/__tests__/cpfvalidator.test.js
@@ -2,16 +2,18 @@ const cpfvalidator = require('../src/validations/cpfvalidator');
 
 describe('cpf validation test', () => {
     it('should valid an cpf', () => {
-        const s = 99438845410;
+        const s = "99438845410";
         const c = cpfvalidator(s);
         expect(c).not.toBeNull();
-        expect(c).toBe(99438845410);
+        expect(c).toBe("994.388.454-10");
 
-    })
-    it('shouldn´t valid an false cpf', () => {
-        const s = 99438845411;
-        const c = cpfvalidator(s);
-        expect(c).not.toBeNull();
-        expect(c).toBe(false);
+    }),
+    it('shouldnt valid an false cpf', () => {
+        try {
+            const s = "99438845411";
+            const c = cpfvalidator(s);
+        } catch (e) {            
+            expect(e.message).toBe('this cpf is invalid');
+        }
     })
 });
