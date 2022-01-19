94c1a6b7a7c56e66ba6346a03ddcb56b83cb49f4
brendo <brendoja@gmail.com>
2022-01-18 21:24:49 -0300

    cadastro de clientes e endereços

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const UserAccessLevelController = require('../controllers/UserAccessLevelController');
-const UserContoller = require('../controllers/UserController');
+const UserController = require('../controllers/UserController');
 const UserStatusController = require('../controllers/UserStatusController');
+const ClientController = require('../controllers/ClientController');
 
 const routes = express.Router();
 
-routes.post('/new_user', UserContoller.store);
+routes.post('/new_user', UserController.store);
 routes.post('/new_userAccess', UserAccessLevelController.store);
 routes.post('/new_userStatus', UserStatusController.store);
-routes.post('/login', UserContoller.loginCheck);
+routes.post('/login', UserController.loginCheck);
+routes.post('/create_user', ClientController.store);
 
 
 module.exports = routes; 
