4feb9107ba34054e3e39c9f6c8b1a5c20caca3b6
brendo <brendoja@gmail.com>
2022-03-27 06:17:24 -0300

    init createToken

--- backend/controllers/CheckUserAuth.js
+++ backend/controllers/CheckUserAuth.js
@@ -1,17 +1,47 @@
 const jwt = require('jsonwebtoken')
 
-function verifyJWT(req, res, next) {
-    var token = req.body.token
-    if (!token) {
-        return res.status(401).send({ auth: false, message: "token null" })
-    }
-    var pubKey = FileSystem.readFiles('../src/keys/public.key', 'utf8')
-    jwt.verify(token, pubKey, { algorithm: ["RS256"] }, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ auth: false, message: "forbidden" })
+export default Jwt = {
+    verifyJWT: (req, res, next) => {
+        var token = req.body.token
+        if (!token) {
+            return res.status(401).send({
+                auth: false,
+                message: "token null"
+            })
         }
-        res.userId = decoded.id
-        next()
-    })
+        var pubKey = FileSystem.readFiles('../src/keys/public.key', 'utf8')
+        jwt.verify(token, pubKey, {
+            algorithm: ["RS256"]
+        }, (err, decoded) => {
+            if (err) {
+                return res.status(403).send({
+                    auth: false,
+                    message: "forbidden"
+                })
+            }
+            res.userId = decoded.id
+            next()
+        })
+
+    },
+    createToken: (userId, time) => {
+        jwt.sign({
+            data: userId
+        }, FileSystem.readFiles('../src/keys/private.key', 'utf8'), options = {
+            exp
+        }, (err, token) => {
+            if (err) {
+                return {
+                    err: '001',
+                    token: false
+                }
+            } else {
+                return {
+                    err: false,
+                    token: token
+                }
+            }
+        })
+    }
 
 }
