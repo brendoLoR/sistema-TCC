d6600f74ad93ffd7c84dcccc3a97ee9cf9684636
brendoLoR <brendoja@gmail.com>
2022-04-09 18:28:16 -0300

    docker configs

--- backend/src/database/database.dev.js
+++ backend/src/database/database.dev.js
@@ -3,9 +3,9 @@
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
-  username: 'postgres',
-  password: 'root',
-  database: 'tcc-cliente-vendas',
+  username: 'system',
+  password: 'system',
+  database: 'tcc',
   define: {
     timestamp: true,
     underscored: true
